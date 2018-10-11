040200b1160b02a8037732d307cbcc3133bf7a7f
omidghafori <cheeesebrain@gmail.com>
2018-10-11 19:29:49 -0400

    completed slideshow

--- source/functions.js
+++ source/functions.js
@@ -1,14 +1,45 @@
-// function sleep(ms) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-// function sleep(milliseconds) {
-//   var start = new Date().getTime();
-//   for (var i = 0; i < 1e7; i++) {
-//     if ((new Date().getTime() - start) > milliseconds){
-//       break;
-//     }
-//   }
-// }
+// waits the specified number of miliseconds
+// call by making function async and using await sleep(ms)
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// switches to specified image in slideshow
+// used by autoswitch as well
+
+function Switch(y) {
+  $('.active-img').removeClass('active-img');
+  $('.img-cont .slide-img:nth-child(' + y + ')').addClass('active-img');
+
+  $('.active-button').removeClass('active-button');
+  $('.button-cont .big-button:nth-child(' + y + ') .button').addClass('active-button');
+}
+
+// automatically switches images with sexy as heck effects
+
+async function autoSwitch() {
+
+  for (var i = 2; i < 100; i++) {
+    $('.loading').css('width', '300px');
+    await sleep(6000);
+
+    if (i % 3 == 0) {
+      Switch(3);
+      console.log("3");
+    }
+    else if ((i-2) % 3 == 0 || i == 2) {
+      Switch(2);
+      console.log("2");
+    }
+    else {
+      Switch(1);
+      console.log("1");
+    }
+    $('.loading').css('transition', '1s ease-out');
+    $('.loading').css('width', '0px');
+    await sleep(1000)
+    $('.loading').css('transition', '6s ease-in');
+  }
+
 }
--- source/main.js
+++ source/main.js
@@ -2,36 +2,4 @@
 pics = ['peng1.jpg', 'peng2.jpg', 'peng3.jpg'];
 // $('.loading').css('width', '300px');
 
-function Switch(y) {
-  $(".slide-img").css("opacity", "0");
-  $(".slide-img").attr("src", "source/peng" + y + ".jpg");
-  $(".slide-img").css("opacity", "1");
-  $('.active').removeClass('active');
-  $('.button-cont .big-button:nth-child(' + y + ') .button').addClass('active');
-}
-async function autoSwitch() {
-
-  for (var i = 2; i < 100; i++) {
-    $('.loading').css('width', '300px');
-    await sleep(6000);
-
-    if (i % 3 == 0) {
-      Switch(3);
-      console.log("3");
-    }
-    else if ((i-2) % 3 == 0 || i == 2) {
-      Switch(2);
-      console.log("2");
-    }
-    else {
-      Switch(1);
-      console.log("1");
-    }
-    $('.loading').css('transition', '1s ease-in');
-    $('.loading').css('width', '0px');
-    await sleep(1000)
-    $('.loading').css('transition', '6s ease-in');
-  }
-
-}
 autoSwitch();
