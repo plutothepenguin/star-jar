17a64827a5f6410b423109398f829383ff260c7d
omidghafori <cheeesebrain@gmail.com>
2018-10-14 19:47:35 -0400

    added logo, moved header to top

--- source/main.js
+++ source/main.js
@@ -1,14 +1,26 @@
 $(document).scroll(function() {
   var scrollTop = $(document).scrollTop();
-  var offsetTop = $('.top').offset().top - 70;
+  var offsetTop = $('.top').offset().top;
   var height = $(".top").height();
 
   if( (offsetTop + height) <= scrollTop){
     $(".header").addClass('sticky');
-    $(".header-item").addClass('header-sticky');
+    $(".header-item").addClass('header-item-sticky');
+    $(".logo-cont").addClass('logo-sticky');
+    $('.header-item-cont').addClass('header-item-cont-sticky');
   }
   else{
     $(".header").removeClass('sticky');
-    $(".header-item").removeClass('header-sticky');
+    $(".header-item").removeClass('header-item-sticky');
+    $(".logo-cont").removeClass('logo-sticky');
+    $('.header-item-cont').removeClass('header-item-cont-sticky');
   }
+});
+var container = $('html, body');
+var scrollTo = $('#mid-1');
+
+$(".title-cont").click(function() {
+  container.animate({
+    scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
+  });
 });
